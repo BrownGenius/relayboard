19c872aa665a013e526997cb35e36a60f3a60693
Nilesh Agarwalla <nilesh@alum.mit.edu>
2022-02-08 12:06:23 -0500

    Added I2C/GPIO Support with Same code base.  Renamed to "relayboard"

--- relayboard.ino
+++ relayboard.ino
@@ -1,5 +1,80 @@
-#include <ArduinoJson.h>
 
+#define BUILD_TYPE_GPIO 1
+#define BUILD_TYPE_I2C 0
+
+#include <ArduinoJson.h>
+/* Required the following Arduino Libraries
+ - ArduinoJSON (v6.19.1)
+*/
+
+
+#if BUILD_TYPE_I2C
+
+#define MODEL "I2C Relay Board"
+
+#include <Wire.h>
+#include <Adafruit_MCP23X08.h>
+
+/* Required the following Arduino Libraries
+ - Adafruit MCP23017 library (v2.0.2)
+*/
+
+// I2C Relayboard test
+// connect VDD to power 5V
+// connect GND to power GND
+// connect SDA to digital SDA (I2C DATA)
+// connect SCL to digital ACL (I2C CLOCK)
+
+#define RELAY_COUNT 8
+#define RELAY_ENABLE 0
+#define RELAY_DISABLE 1
+#define I2C_BOARD_COUNT 1
+
+uint8_t relay2PinMapping[RELAY_COUNT] = {
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+#if 0
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7
+#endif
+};
+
+uint8_t relay2BoardMapping[RELAY_COUNT] = {
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+#if 0
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+#endif
+};
+
+Adafruit_MCP23X08 mcp[I2C_BOARD_COUNT];
+#else
+#define MODEL "GPIO Relay Board"
 // GPIO Relayboard test
 // connect VDD to power 5V
 // connect GND to power GND
@@ -26,6 +101,7 @@ uint8_t relay2PinMapping[RELAY_COUNT] = {
     2,  /* Relay e (14) */
     A5, /* Relay f (15) */
 };
+#endif
 
 /* JSON Format */
 #if 0
@@ -45,9 +121,13 @@ uint8_t relay2PinMapping[RELAY_COUNT] = {
 
 
 {"relays":[{"port":0,"enable":false},{"port":1,"enable":true},{"port":2,"enable":false},{"port":3,"enable":true},{"port":4,"enable":true},{"port":5,"enable":true},{"port":6,"enable":true},{"port":7,"enable":true},{"port":8,"enable":true},{"port":9,"enable":true},{"port":10,"enable":true},{"port":11,"enable":true},{"port":12,"enable":true},{"port":13,"enable":true},{"port":14,"enable":true},{"port":15,"enable":true}]}
+
+{"relays":[{"port":0,"enable":false},{"port":1,"enable":false}]}
+
+{"relays":[{"port":2,"enable":false},{"port":3,"enable":false}]}
 #endif
 
-#define VERSION 0x20220206
+#define VERSION 0x20220207
 
 StaticJsonDocument<32*RELAY_COUNT> status;
 StaticJsonDocument<32*RELAY_COUNT> input;
@@ -56,7 +136,7 @@ void printInfo()
 {
     StaticJsonDocument<256> info;
 
-    info["model"] = "GPIO Relay Board";
+    info["model"] = MODEL;
     info["version"] = VERSION;
     info["relay count"] = RELAY_COUNT;
 
@@ -73,7 +153,11 @@ void printStatus()
 void setRelay(uint8_t relay, bool enable)
 {
     /* TODO: Validate relay */
+#if BUILD_TYPE_I2C
+    mcp[relay2BoardMapping[relay]].digitalWrite(relay2PinMapping[relay], enable ? RELAY_ENABLE : RELAY_DISABLE);
+#else
     digitalWrite(relay2PinMapping[relay], enable ? RELAY_ENABLE : RELAY_DISABLE);
+#endif
     status["relays"][relay]["port"] = relay;
     status["relays"][relay]["enable"] = enable;
 }
@@ -82,22 +166,37 @@ void initRelays()
 {
     uint8_t relay;
 
+#if BUILD_TYPE_I2C
+    uint8_t board;
+    for (board = 0; board < I2C_BOARD_COUNT; board++)
+    {
+        Serial.println("configuring MCP board");
+//        mcp[board].begin_I2C(board);
+    }
+#elif BUILD_TYPE_GPIO
     for (relay = 0; relay < RELAY_COUNT; relay++)
     {
         pinMode(relay2PinMapping[relay], OUTPUT);
+    }
+#endif
+
+    for (relay = 0; relay < RELAY_COUNT; relay++)
+    {
         setRelay(relay, true);
     }
 }
 
 void setup()
 {
+    // start serial port at 9600 bps and wait for port to open:
+    Serial.begin(9600);
+
+    Serial.println("Initializing...");
+
     status.createNestedArray("relays");
     initRelays();
 
-    // start serial port at 9600 bps and wait for port to open:
-    Serial.begin(9600);
-    while (!Serial)
-        ; // wait for serial port to connect. Needed for Leonardo only
+    Serial.println("Initialized!");
 }
 
 void loop()
